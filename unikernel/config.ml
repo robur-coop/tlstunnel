3302a534ce73c435f53b829380448d5c38326e61
Hannes Mehnert <hannes@mehnert.org>
2023-11-23 16:20:45 +0100

    add --use-utcp configuration key

--- unikernel/config.ml
+++ unikernel/config.ml
@@ -1,3 +1,4 @@
+(* mirage >= 4.4.1 & < 4.5.0 *)
 (* (c) 2019 Hannes Mehnert, all rights reserved *)
 
 open Mirage
@@ -49,10 +50,50 @@ let main =
     "Unikernel.Main"
     (random @-> time @-> pclock @-> block @-> stackv4v6 @-> stackv4v6 @-> job)
 
-let stack = generic_stackv4v6 default_network
+(* uTCP *)
+
+let tcpv4v6_direct_conf id =
+  let packages_v = Key.pure [ package "utcp" ~sublibs:[ "mirage" ] ] in
+  let connect _ modname = function
+    | [_random; _mclock; _time; ip] ->
+      Fmt.str "Lwt.return (%s.connect %S %s)" modname id ip
+    | _ -> failwith "direct tcpv4v6"
+  in
+  impl ~packages_v ~connect "Utcp_mirage.Make"
+    (random @-> mclock @-> time @-> ipv4v6 @-> (tcp: 'a tcp typ))
+
+let direct_tcpv4v6
+    ?(clock=default_monotonic_clock)
+    ?(random=default_random)
+    ?(time=default_time) id ip =
+  tcpv4v6_direct_conf id $ random $ clock $ time $ ip
+
+let net ?group name netif =
+  let ethernet = etif netif in
+  let arp = arp ethernet in
+  let i4 = create_ipv4 ?group ethernet arp in
+  let i6 = create_ipv6 ?group netif ethernet in
+  let i4i6 = create_ipv4v6 ?group i4 i6 in
+  let tcpv4v6 = direct_tcpv4v6 name i4i6 in
+  let ipv4_only = Key.ipv4_only ?group () in
+  let ipv6_only = Key.ipv6_only ?group () in
+  direct_stackv4v6 ~tcp:tcpv4v6 ~ipv4_only ~ipv6_only netif ethernet arp i4 i6
+
+let use_utcp =
+  let doc = Key.Arg.info ~doc:"Use uTCP" [ "use-utcp" ] in
+  Key.(create "use-utcp" Arg.(flag ~stage:`Configure doc))
+
+let stack =
+  if_impl
+    (Key.value use_utcp)
+    (net "service" default_network)
+    (generic_stackv4v6 default_network)
 
 let private_stack =
-  generic_stackv4v6 ~group:"private" (netif ~group:"private" "private")
+  if_impl
+    (Key.value use_utcp)
+    (net ~group:"private" "private" (netif ~group:"private" "private"))
+    (generic_stackv4v6 ~group:"private" (netif ~group:"private" "private"))
 
 let block =
   Key.(if_impl is_solo5 (block_of_file "storage") (block_of_file "disk.img"))
@@ -67,7 +108,10 @@ let enable_monitoring =
 let management_stack =
   if_impl
     (Key.value enable_monitoring)
-    (generic_stackv4v6 ~group:"management" (netif ~group:"management" "management"))
+    (if_impl
+       (Key.value use_utcp)
+       (net ~group:"management" "management" (netif ~group:"management" "management"))
+       (generic_stackv4v6 ~group:"management" (netif ~group:"management" "management")))
     stack
 
 let name =
