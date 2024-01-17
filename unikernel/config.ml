ec2a700f25de0386d902dd6e4c4eeae96090db5c
Thomas Gazagnaire <thomas@gazagnaire.org>
2024-01-17 18:21:17 +0100

    Use latest mirage main

--- unikernel/config.ml
+++ unikernel/config.ml
@@ -3,44 +3,11 @@
 
 open Mirage
 
-let frontend_port =
-  let doc = Key.Arg.info ~doc:"The TCP port of the frontend." ["frontend-port"] in
-  Key.(create "frontend_port" Arg.(opt int 443 doc))
-
-let key =
-  let doc = Key.Arg.info ~doc:"The shared secret" ["key"] in
-  Key.(create "key" Arg.(required string doc))
-
-let configuration_port =
-  let doc = Key.Arg.info ~doc:"The TCP port for configuration." ["configuration-port"] in
-  Key.(create "configuration_port" Arg.(opt int 1234 doc))
-
-let dns_key =
-  let doc = Key.Arg.info ~doc:"nsupdate key (name:type:value,...)" ["dns-key"] in
-  Key.(create "dns-key" Arg.(required string doc))
-
-let dns_server =
-  let doc = Key.Arg.info ~doc:"dns server IP" ["dns-server"] in
-  Key.(create "dns-server" Arg.(required ip_address doc))
-
-let domains =
-  let doc = Key.Arg.info ~doc:"domains" ["domains"] in
-  Key.(create "domains" Arg.(required (list string) doc))
-
-let key_seed =
-  let doc = Key.Arg.info ~doc:"certificate key seed" ["key-seed"] in
-  Key.(create "key-seed" Arg.(required string doc))
+let setup = runtime_key ~pos:__POS__ "Unikernel.K.setup"
 
 let main =
-  foreign
-    ~keys:[ Key.v frontend_port ;
-            Key.v key ;
-            Key.v configuration_port ;
-            Key.v dns_key ;
-            Key.v dns_server ;
-            Key.v domains ;
-            Key.v key_seed ;
-          ]
+  main
+    ~runtime_keys:[setup]
     ~packages:[
       package ~min:"0.14.0" "tls-mirage" ;
       package ~min:"5.0.1" ~sublibs:["mirage"] "dns-certify" ;
@@ -76,13 +43,13 @@ let net ?group name netif =
   let i6 = create_ipv6 ?group netif ethernet in
   let i4i6 = create_ipv4v6 ?group i4 i6 in
   let tcpv4v6 = direct_tcpv4v6 name i4i6 in
-  let ipv4_only = Key.ipv4_only ?group () in
-  let ipv6_only = Key.ipv6_only ?group () in
+  let ipv4_only = Runtime_key.ipv4_only ?group () in
+  let ipv6_only = Runtime_key.ipv6_only ?group () in
   direct_stackv4v6 ~tcp:tcpv4v6 ~ipv4_only ~ipv6_only netif ethernet arp i4 i6
 
 let use_utcp =
-  let doc = Key.Arg.info ~doc:"Use uTCP" [ "use-utcp" ] in
-  Key.(create "use-utcp" Arg.(flag ~stage:`Configure doc))
+  let doc = Cmdliner.Arg.info ~doc:"Use uTCP" [ "use-utcp" ] in
+  Key.(create "use-utcp" Arg.(flag doc))
 
 let stack =
   if_impl
@@ -100,11 +67,11 @@ let block =
   Key.(if_impl is_solo5 (block_of_file "storage") (block_of_file "disk.img"))
 
 let enable_monitoring =
-  let doc = Key.Arg.info
+  let doc = Cmdliner.Arg.info
       ~doc:"Enable monitoring (only available for solo5 targets)"
       [ "enable-monitoring" ]
   in
-  Key.(create "enable-monitoring" Arg.(flag ~stage:`Configure doc))
+  Key.(create "enable-monitoring" Arg.(flag doc))
 
 let management_stack =
   if_impl
@@ -116,56 +83,45 @@ let management_stack =
     stack
 
 let name =
-  let doc = Key.Arg.info ~doc:"Name of the unikernel" [ "name" ] in
-  Key.(v (create "name" Arg.(opt string "roburtls" doc)))
+  runtime_key ~pos:__POS__
+    {|let open Cmdliner in
+      let doc = Arg.info ~doc:"Name of the unikernel" [ "name" ] in
+      Arg.(value & opt string "roburtls" doc)|}
 
 let monitoring =
-  let monitor =
-    let doc = Key.Arg.info ~doc:"monitor host IP" ["monitor"] in
-    Key.(v (create "monitor" Arg.(opt (some ip_address) None doc)))
-  in
+  let monitor = Runtime_key.(v (monitor None)) in
   let connect _ modname = function
-    | [ _ ; _ ; stack ] ->
-      Fmt.str "Lwt.return (match %a with\
+    | [ _ ; _ ; stack; name; monitor ] ->
+      Fmt.str "Lwt.return (match %s with\
                | None -> Logs.warn (fun m -> m \"no monitor specified, not outputting statistics\")\
-               | Some ip -> %s.create ip ~hostname:%a %s)"
-        Key.serialize_call monitor modname
-        Key.serialize_call name stack
+               | Some ip -> %s.create ip ~hostname:%s %s)"
+        monitor modname name stack
     | _ -> assert false
   in
   impl
     ~packages:[ package "mirage-monitoring" ]
-    ~keys:[ name ; monitor ]
+    ~runtime_keys:[ name ; monitor ]
     ~connect "Mirage_monitoring.Make"
     (time @-> pclock @-> stackv4v6 @-> job)
 
 let syslog =
-  let syslog =
-    let doc = Key.Arg.info ~doc:"syslog host IP" ["syslog"] in
-    Key.(v (create "syslog" Arg.(opt (some ip_address) None doc)))
-  in
+  let syslog = Runtime_key.(v (syslog None)) in
   let connect _ modname = function
-    | [ _ ; stack ] ->
-      Fmt.str "Lwt.return (match %a with\
+    | [ _ ; stack; name; syslog ] ->
+      Fmt.str "Lwt.return (match %s with\
                | None -> Logs.warn (fun m -> m \"no syslog specified, dumping on stdout\")\
-               | Some ip -> Logs.set_reporter (%s.create %s ip ~hostname:%a ()))"
-        Key.serialize_call syslog modname stack
-        Key.serialize_call name
+               | Some ip -> Logs.set_reporter (%s.create %s ip ~hostname:%s ()))"
+        syslog modname stack name
     | _ -> assert false
   in
   impl
     ~packages:[ package ~sublibs:["mirage"] ~min:"0.4.0" "logs-syslog" ]
-    ~keys:[ name ; syslog ]
+    ~runtime_keys:[ name ; syslog ]
     ~connect "Logs_syslog_mirage.Udp"
     (pclock @-> stackv4v6 @-> job)
 
 
-type i0 = I0
-let i0 = Functoria.Type.v I0
 let no0 = Functoria.impl "Int" job
-
-type n1 = N1
-let n1 = Functoria.Type.v N1
 let noop1 = Functoria.impl "Set.Make" (job @-> job)
 
 let optional_monitoring time pclock stack =
